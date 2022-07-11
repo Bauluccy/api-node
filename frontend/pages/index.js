abfdc47fd3651d7d9b9da450346136c7116927e7
Bauluccy <fifoluciano@hotmail.com.br>
2022-07-11 17:03:59 -0300

    beta 0.4

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,29 +1,46 @@
 import Head from "next/head";
 import Link from "next/link";
+import classnames from 'classnames'
+import style from './index.module.css'
+import buttons from './buttons.module.css'
 
-function Home() {
+export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <div className={style.container} >
+        <Head>
+          <title>Create Next App</title>
+          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="stylesheet" href="index.css"/> */}
+        </Head>
 
-      <main>
-        <nav>
-          <ul>
-            <li>
-              <Link href="/users/list">
-                <a>Usuários</a>
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </main>
+        <main>
+          <header>
+            <h1 className={style.header}>Selecione qual tipo de cadastro</h1>
+          </header>
+            <nav className={classnames(
+              style.nav
+            )}>
+              <ul>
+                  <Link href="/users/list">
+                    <button className={classnames(
+                      buttons.button,
+                      buttons.green
+                    )}>Usuários</button>
+                  </Link>
+                  {/* <Link href=>
+                    <a>Carros</a>
+                  </Link> */}
+              </ul>
+            </nav>
+          </main>
 
-      <footer>
-        <p>API Desenvolvida na cadeira de Programação WEB</p>
-      </footer>
-    </div>
+        <footer>
+          <p>API Desenvolvida na cadeira de Programação WEB</p>
+        </footer>
+      </div>
   );
+  
 }
+
+
+
